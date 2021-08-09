3666d915438bfbce06e7ea14919d9b5bd6665608
Diego Musci <diegomusci18@gmail.com>
2021-08-09 19:13:17 -0300

    first commit

---
+++ snike.py
@@ -0,0 +1,22 @@
+import turtle
+
+pluma = turtle.Turtle()
+
+
+pluma.speed(30)
+for i in range(0,4):
+    pluma.forward(100)
+    pluma.left(90)
+
+#cabeza de serpiente
+cabeza = turtle.Turtle()
+cabeza.speed(0)
+cabeza.shape("square")
+cabeza.penup()
+cabeza.goto(0,0)
+
+cabeza.direction = "stop"
+
+while True
+    wn.update()
+
